945ccf1e910d2473d4d6815ad91e5a392b1715a5
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-05-09 22:29:04 +0800

    Debugging logic

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 const outerLayout = document.querySelector("#outer-layout");
 const numButtons = document.querySelector("#num-buttons");
 const operators = document.querySelector("#operators");
+const results = document.querySelector("#results");
+
+
+function appendToResults (input) {
+    results.value += input;
+}
+
+function calculate (num1, num2, operator) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch (operator) {
+        case "+": return num1 + num2;
+        case "-": return num1 - num2;
+        case "*": return num1 * num2;
+        case "/": return num2 !== 0 ? num1 / num2 : "Error";
+        default: return 0;
+    }
+}
