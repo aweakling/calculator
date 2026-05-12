abab10692a9e7fbbcb81ca0f8b3fad278de0b4d0
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-05-12 22:18:41 +0800

    Operate function works and add colors to buttons

--- javascript.js
+++ javascript.js
@@ -1,10 +1,50 @@
 const buttons = document.querySelectorAll(".key");
 const outputDisplay = document.querySelector("#display");
 
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const keyValue = button.textContent;
-        console.log(keyValue);
-        outputDisplay.value += keyValue;
+        operate(keyValue);
     });
 });
+
+function operate (input) {
+    
+    if(input === "C"){
+        outputDisplay.value = "";
+
+        firstNum = "";
+        secondNum = "";
+        operator = "";
+
+    } else if (input === "+" || input === "-" || input === "*" || input === "/" ) {
+        operator = input;
+
+        firstNum = outputDisplay.value;
+
+        outputDisplay.value = "";
+    } else if (input === "=") {
+        secondNum = outputDisplay.value;
+
+        if (operator === "+") {
+            outputDisplay.value = Number(firstNum) + Number(secondNum);
+        }
+        if (operator === "-") {
+            outputDisplay.value = Number(firstNum) - Number(secondNum);
+        }
+        if (operator === "*") {
+            outputDisplay.value = Number(firstNum) * Number(secondNum);
+        }
+        if (operator === "/") {
+            outputDisplay.value = Number(firstNum) / Number(secondNum);
+        }
+    }
+
+    else {
+        outputDisplay.value += input;
+    }
+}
