ead116325e6e8199e80458baaa754388bc4862f6
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-05-13 21:14:25 +0800

    Upgrade logic

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const outputDisplay = document.querySelector("#display");
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+let shouldResetDisplay = false;
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
@@ -12,40 +13,74 @@ buttons.forEach(button => {
     });
 });
 
+function calculate (num1, num2, operator) {
+
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if(operator === "+") return num1 + num2;
+    if(operator === "-") return num1 - num2;
+    if(operator === "x") return num1 * num2;
+    if(operator === "/") return num1 / num2;
+}
+
 function operate (input) {
     
     if(input === "C"){
         outputDisplay.value = "";
-
         firstNum = "";
         secondNum = "";
         operator = "";
+        return;
+    } 
 
-    } else if (input === "+" || input === "-" || input === "x" || input === "/" ) {
+    if(!isNaN(input)){
+
+        if(shouldResetDisplay){
+            outputDisplay.value = "";
+            shouldResetDisplay = false;
+        }
+
+        outputDisplay.value += input;
+        return;
+    }
+
+    if (["+", "-", "x", "/"].includes(input)) {
+        
+        if(firstNum === ""){
+            firstNum = outputDisplay.value;
+
+        } else if(operator !== ""){
+
+            secondNum = outputDisplay.value.split(operator).pop();
+
+            firstNum = calculate(firstNum, secondNum, operator);
+           
+            outputDisplay.value = firstNum;
+
+        }
+        
         operator = input;
 
-        firstNum = outputDisplay.value;
-
-        outputDisplay.value = "";
-
-    } else if (input === "=") {
-        secondNum = outputDisplay.value;
-
-        if (operator === "+") {
-            outputDisplay.value = Number(firstNum) + Number(secondNum);
-        }
-        if (operator === "-") {
-            outputDisplay.value = Number(firstNum) - Number(secondNum);
-        }
-        if (operator === "x") {
-            outputDisplay.value = Number(firstNum) * Number(secondNum);
-        }
-        if (operator === "/") {
-            outputDisplay.value = Number(firstNum) / Number(secondNum);
-        }
-    }
-
-    else {
         outputDisplay.value += input;
+        return;
     }
+    
+    if (input === "=") {
+        
+        if(firstNum === "" || operator === "") return;
+
+        secondNum = outputDisplay.value.split(operator).pop().trim();
+
+        const result = calculate(firstNum, secondNum, operator);
+
+        outputDisplay.value = result;
+
+        firstNum = result;
+        secondNum = "";
+        operator = "";
+        shouldResetDisplay = true;
+    }
+
+    outputDisplay.value += input;
 }
