59f412de41ed373dee27d2490842e615bb8198a3
Aweakling <173584192+aweakling@users.noreply.github.com>
2026-05-20 00:16:26 +0800

    Add decimal logic and fix divide 0 error

--- javascript.js
+++ javascript.js
@@ -1,13 +1,23 @@
 const buttons = document.querySelectorAll(".key");
+const operatorButtons = document.querySelectorAll(".key.operator");
 const outputDisplay = document.querySelector("#display");
 
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+let resetDisplay = false;
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const keyValue = button.textContent;
+        
+        operatorButtons.forEach(btn => {
+            btn.style.backgroundColor = "orange";
+        });
+        if(button.classList.contains("operator")){
+            button.style.backgroundColor = "pink";
+        }
+
         calculate(keyValue);
     });
 });
@@ -17,10 +27,17 @@ function operate (num1, num2, operator) {
     num1 = Number(num1);
     num2 = Number(num2);
 
-    if(operator === "+") return num1 + num2;
-    if(operator === "-") return num1 - num2;
-    if(operator === "x") return num1 * num2;
-    if(operator === "/") return num1 / num2;
+    let result;
+    
+    if(operator === "+") result = num1 + num2;
+    if(operator === "-") result = num1 - num2;
+    if(operator === "x") result = num1 * num2;
+    if(operator === "/") {
+        if(num2 === 0) return "Error";
+        result = num1 / num2;
+    }
+
+    return Number(result.toFixed(2));
 }
 
 function calculate (input) {
@@ -30,26 +47,59 @@ function calculate (input) {
         firstNum = "";
         secondNum = "";
         operator = "";
+        resetDisplay = false;
         return;
     }
 
-    if(input === "") return outputDisplay.value = " ";
-
     if((["+", "-", "x","/"]).includes(input)){
-        firstNum = outputDisplay.value;
+
+        if(operator !== "" && outputDisplay.value === "") return;
+
+        if(firstNum !== "" && operator !== ""){
+            secondNum = outputDisplay.value;
+            result = operate(firstNum, secondNum, operator);
+            outputDisplay.value = result;
+            firstNum = result;
+        } else {
+            firstNum = outputDisplay.value;
+        }
         operator = input;
-        outputDisplay.value = "";
+        resetDisplay = true;
         return;
+    }
+
+    if(input === "."){
+        if(outputDisplay.value.includes(".")){
+            return;
+        }
+
+        if(outputDisplay.value === ""){
+            outputDisplay.value = "0"
+        }
     }
 
     if(input === "="){
+        
+        if (firstNum === "" || operator === "") return;
+        
         secondNum = outputDisplay.value;
 
         let result = operate(firstNum, secondNum, operator);
+        
+        if(result === "Error"){
+            outputDisplay.value = "Cannot divide by 0"
+        } else {
+            outputDisplay.value = result;
+        }
 
-        outputDisplay.value = result;
+        firstNum = "";
+        operator = "";
         return;
     }
 
+    if(resetDisplay){
+        outputDisplay.value = "";
+        resetDisplay = false;
+    }   
     outputDisplay.value += input;
 }
